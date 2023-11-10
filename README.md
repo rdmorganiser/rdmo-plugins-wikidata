f891d506eef5eb8c73fe9e7c583498c1cf093b74
Jochen Klar <mail@jochenklar.de>
2023-11-10 16:45:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+rdmo-wikidata
+=============
+
+This plugin implements dynamic option set, that queries the Wikidata Search API at https://www.wikidata.org/w/api.php. See [Wikidata:Data_access](https://www.wikidata.org/wiki/Wikidata:Data_access) for more information about the different Wikidata endpoints. Please also note the access best practices on the same page.
+
+Setup
+-----
+
+Install the plugin in your RDMO virtual environment using pip (directly from GitHub):
+
+```bash
+pip install git+https://github.com/rdmorganiser/rdmo-wikidata
+```
+
+Add the plugin to `OPTIONSET_PROVIDERS` in `config/settings/local.py`:
+
+```python
+OPTIONSET_PROVIDERS += [
+    ('wikidata', _('Wikidata Provider'), 'rdmo_wikidata.providers.WikidataProvider')
+]
+```
+
+The option set provider should now be selectable for option sets in your RDMO installation. For a minimal example catalog, see the files in `xml`.
+
+Wikidata's policy asks to add a custom `User-Agent` to your requests so that they can perform statistical analyses and, if you add an email address, might contact you. This can be done by adding the following to your settings.
+
+```python
+WIKIDATA_PROVIDER_HEADERS = {
+    'User-Agent': 'rdmo.example.com/0.0 (mail@rdmo.example.com) rdmo-wikidata/1.0'
+}
+```
---
+++ rdmo_wikidata/providers.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.utils.html import strip_tags
+
+import dpath
+import requests
+
+from rdmo.options.providers import Provider
+
+
+class WikidataProvider(Provider):
+
+    search = True
+
+    def get_options(self, project, search=None):
+        if search:
+            url = getattr(settings, 'WIKIDATA_PROVIDER_URL', 'https://www.wikidata.org/w/api.php')
+            headers = getattr(settings, 'WIKIDATA_PROVIDER_HEADERS', {})
+            response = requests.get(f'{url}/search', params={
+                'action': 'query',
+                'list': 'search',
+                'srsearch': search,
+                'srprop': 'titlesnippet|snippet',
+                'format': 'json'
+            }, headers=headers)
+
+            try:
+                data = response.json()
+            except requests.exceptions.JSONDecodeError:
+                pass
+            else:
+                try:
+                    items = dpath.get(data, 'query/search')
+                except KeyError:
+                    pass
+                else:
+                    return [
+                        {
+                            'id': item['title'],
+                            'text': strip_tags('{titlesnippet} ({snippet}) [{title}]'.format(**item))
+                        } for item in items
+                    ]
+
+        # return an empty list by default
+        return []
